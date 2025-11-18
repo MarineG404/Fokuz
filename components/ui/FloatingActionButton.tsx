5a46ff0404607175c121a3258d1dafae7ee03fee
MarineG404 <marine.gonnord.pro@gmail.com>
2025-11-18 11:11:16 +0100

    💄 Update FloatingActionButton border radius

--- components/ui/FloatingActionButton.tsx
+++ components/ui/FloatingActionButton.tsx
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
 		right: 24,
 		width: 64,
 		height: 64,
-		borderRadius: SPACING.radius["3xl"],
+		borderRadius: SPACING.radius.full,
 		alignItems: "center",
 		justifyContent: "center",
 		elevation: 8,
